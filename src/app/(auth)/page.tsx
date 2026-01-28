8620da95ad2e189fa67de5a2a4e625451a6198d4
Thiago Menezes <thiago.mhx@gmail.com>
2026-01-28 11:53:51 -0500

    refactor: remove 'use client' directive from authentication page components.

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { Dashboard } from '@/features/dashboard';
 
 const DashboardPage = () => {
--- src/app/(auth)/workspaces/[workspaceSlug]/page.tsx
+++ src/app/(auth)/workspaces/[workspaceSlug]/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import WorkspaceDetailPage from '@/features/projects';
 
 const Page = () => {
